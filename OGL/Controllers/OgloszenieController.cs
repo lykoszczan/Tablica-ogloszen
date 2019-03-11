8a35f357b49d0a3fed937817d5a5df0c9e18d919
Marcin Łykowski <marcin.lykowski@medialand.pl>
2019-03-11 12:14:07 +0100

    Poprawa architektury aplikacji - zastosowanie kontenerów IoC

--- OGL/Controllers/OgloszenieController.cs
+++ OGL/Controllers/OgloszenieController.cs
@@ -7,132 +7,134 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Repozytorium.IRepo;
 using Repozytorium.Models;
+using Repozytorium.Repo;
 
 namespace OGL.Controllers
 {
     public class OgloszenieController : Controller
     {
-        private OglContext db = new OglContext();
-
+        private readonly IOgloszenieRepo _repo;
+        
+        public OgloszenieController(IOgloszenieRepo repo)
+        {
+            _repo = repo;
+        }
+        
         // GET: Ogloszenies
         public ActionResult Index()
         {
-            //debug
-            db.Database.Log = message => Trace.WriteLine(message);
-
-            // optymalizacja
-            // var ogloszenia = db.Ogloszenia.Include(o => o.Uzytkownik);
-            var ogloszenia = db.Ogloszenia.AsNoTracking();
-            return View(ogloszenia.ToList());
+            var ogloszenia = _repo.PobierzOgloszenia();
+            return View(ogloszenia);
         }
 
-        // GET: Ogloszenies/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
-            if (ogloszenie == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ogloszenie);
-        }
+        //// GET: Ogloszenies/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
+        //    if (ogloszenie == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(ogloszenie);
+        //}
 
-        // GET: Ogloszenies/Create
-        public ActionResult Create()
-        {
-            ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email");
-            return View();
-        }
+        //// GET: Ogloszenies/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email");
+        //    return View();
+        //}
 
-        // POST: Ogloszenies/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Tresc,Tytul,DataDodania,UzytkownikId")] Ogloszenie ogloszenie)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Ogloszenia.Add(ogloszenie);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        //// POST: Ogloszenies/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "Id,Tresc,Tytul,DataDodania,UzytkownikId")] Ogloszenie ogloszenie)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Ogloszenia.Add(ogloszenie);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
 
-            ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email", ogloszenie.UzytkownikId);
-            return View(ogloszenie);
-        }
+        //    ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email", ogloszenie.UzytkownikId);
+        //    return View(ogloszenie);
+        //}
 
-        // GET: Ogloszenies/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
-            if (ogloszenie == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email", ogloszenie.UzytkownikId);
-            return View(ogloszenie);
-        }
+        //// GET: Ogloszenies/Edit/5
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
+        //    if (ogloszenie == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email", ogloszenie.UzytkownikId);
+        //    return View(ogloszenie);
+        //}
 
-        // POST: Ogloszenies/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Tresc,Tytul,DataDodania,UzytkownikId")] Ogloszenie ogloszenie)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(ogloszenie).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email", ogloszenie.UzytkownikId);
-            return View(ogloszenie);
-        }
+        //// POST: Ogloszenies/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Tresc,Tytul,DataDodania,UzytkownikId")] Ogloszenie ogloszenie)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(ogloszenie).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.UzytkownikId = new SelectList(db.Users, "Id", "Email", ogloszenie.UzytkownikId);
+        //    return View(ogloszenie);
+        //}
 
-        // GET: Ogloszenies/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
-            if (ogloszenie == null)
-            {
-                return HttpNotFound();
-            }
-            return View(ogloszenie);
-        }
+        //// GET: Ogloszenies/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
+        //    if (ogloszenie == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(ogloszenie);
+        //}
 
-        // POST: Ogloszenies/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
-            db.Ogloszenia.Remove(ogloszenie);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Ogloszenies/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Ogloszenie ogloszenie = db.Ogloszenia.Find(id);
+        //    db.Ogloszenia.Remove(ogloszenie);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
     }
 }
---
+++ Repozytorium/Repo/OgloszenieRepo.cs
@@ -0,0 +1,30 @@
+﻿using Repozytorium.IRepo;
+using Repozytorium.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace Repozytorium.Repo
+{
+    public class OgloszenieRepo : IOgloszenieRepo
+    {
+        private IOglContext _db = new OglContext();
+
+
+        public OgloszenieRepo(IOglContext db)
+        {
+            _db = db;
+        }
+
+        //zla praktyka, nie powinno sie zwracac IQueryable, poniewaz wtedy filtrowanie wynikow odbywa sie poza repo i wtedy kod nie jest testowalny w 100%
+        //powinno sie zwracac IEnumerable i jako parametr jakis filtr
+        public IQueryable<Ogloszenie> PobierzOgloszenia()
+        {
+            _db.Database.Log = message => Trace.WriteLine(message);
+            var ogloszenie = _db.Ogloszenia.AsNoTracking();
+            return ogloszenie;
+        }
+    }
+}
